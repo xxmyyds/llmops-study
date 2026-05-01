b26fba5415d0838a49c82048c7afbd2051e8d51c
xxmyyds <code.xxmyyds@gmail.com>
2026-05-01 20:01:02 +0800

    runnable自定义记忆实体存储

--- internal/handler/app_handler.py
+++ internal/handler/app_handler.py
@@ -6,14 +6,17 @@
 import uuid
 from dataclasses import dataclass
 from operator import itemgetter
+from typing import Dict, Any
 from uuid import UUID
 
 from injector import inject
+from langchain_classic.base_memory import BaseMemory
 from langchain_classic.memory import ConversationBufferWindowMemory
 from langchain_community.chat_message_histories import FileChatMessageHistory
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda, RunnableConfig
+from langchain_core.tracers import Run
 from langchain_openai import ChatOpenAI
 
 from internal.exception import UnauthorizedException
@@ -43,6 +46,23 @@ class AppHandler:
         app = self.app_service.delete_app(id)
         return success_message(f"应用已被删除, id: {app.id}")
 
+    @classmethod
+    def _load_memory_variables(cls, input: Dict[str, Any], config: RunnableConfig) -> Dict[str, Any]:
+        """加载记忆变量信息"""
+        configurable = config.get("configurable", {})
+        configurable_memory = configurable.get("memory", None)
+        if configurable_memory is not None and isinstance(configurable_memory, BaseMemory):
+            return configurable_memory.load_memory_variables(input)
+        return {'history': []}
+
+    @classmethod
+    def _save_context(cls, run_obj: Run, config: RunnableConfig) -> None:
+        """存储对应的上下文信息到记忆实体中"""
+        configurable = config.get("configurable", {})
+        configurable_memory = configurable.get("memory", None)
+        if configurable_memory is not None and isinstance(configurable_memory, BaseMemory):
+            configurable_memory.save_context(run_obj.inputs, run_obj.outputs)
+
     def debug(self, app_id: UUID):
         req = CompletionReq()
         if not req.validate():
@@ -62,13 +82,12 @@ class AppHandler:
 
         llm = ChatOpenAI(model='deepseek-chat')
 
-        chain = RunnablePassthrough.assign(
-            history=RunnableLambda(memory.load_memory_variables) | itemgetter('history'),
-        ) | prompt | llm | StrOutputParser()
+        chain = (RunnablePassthrough.assign(
+            history=RunnableLambda(self._load_memory_variables) | itemgetter('history'),
+        ) | prompt | llm | StrOutputParser()).with_listeners(on_end=self._save_context)
         # 调用链获得结果
         chain_input = {'query': req.query.data}
-        content = chain.invoke(chain_input)
-        memory.save_context(chain_input, {'output': content})
+        content = chain.invoke(chain_input, config={"configurable": {"memory": memory}})
 
         return success_json({"content": content})
 
