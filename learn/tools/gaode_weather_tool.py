6738a4f39f3d08cc09ba2b9f7fd0565a1fa7fe6a
Code_xxmyyds <xxmyyds@163.com>
2026-06-06 11:55:32 +0800

    tools 与 chatModel 使用

--- learn/tools/gaode_weather_tool.py
+++ learn/tools/gaode_weather_tool.py
@@ -8,7 +8,11 @@ from typing import Any, Type
 
 import dotenv
 import requests
+from langchain_core.messages import ToolMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 from langchain_core.tools import BaseTool
+from langchain_openai import ChatOpenAI
 from pydantic import BaseModel, Field
 
 dotenv.load_dotenv()
@@ -59,6 +63,36 @@ class GaodeWeatherTool(BaseTool):
 
 
 gaodeWeatherTool = GaodeWeatherTool()
-res = gaodeWeatherTool.invoke({'city': '上海'})
-print(res)
-print(type(res))
+# res = gaodeWeatherTool.invoke({'city': '上海'})
+# print(res)
+# print(type(res))
+tools_dict = {
+    gaodeWeatherTool.name: gaodeWeatherTool,
+}
+tools = [tool for tool in tools_dict.values()]
+
+prompt = ChatPromptTemplate.from_messages([
+    ('system', '你是一个聊天机器人，请根据用户提出的问题回答，必要时可以调用工具'),
+    ('human', '{query}')
+])
+query = '上海天气怎么样，适合穿什么衣服'
+llm = ChatOpenAI(model='deepseek-chat', temperature=0)
+llm_with_tool = llm.bind_tools(tools=tools)
+
+chain = {"query": RunnablePassthrough()} | prompt | llm_with_tool
+
+res = chain.invoke(query)
+tool_calls = res.tool_calls
+if len(tool_calls) <= 0:
+    print(res.content)
+else:
+    messages = prompt.invoke(query).to_messages()
+    messages.append(res)
+
+    for tool_call in tool_calls:
+        tool = tools_dict.get(tool_call.get('name'))
+        content = tool.invoke(tool_call.get('args'))
+        tool_call_id = tool_call.get('id')
+        messages.append(ToolMessage(tool_call_id=tool_call_id, content=content))
+
+    print('输出内容：', llm_with_tool.invoke(messages))
